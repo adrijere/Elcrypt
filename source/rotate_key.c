15c52c9857c1009c866f09633bf6343f75e13922
Valentin Cardon <valentin.cardon@epitech.eu>
2015-03-15 17:57:26 +0100

    up

--- source/rotate_key.c
+++ source/rotate_key.c
@@ -5,7 +5,7 @@
 ** Login   <cardon_v@epitech.net>
 ** 
 ** Started on  Sun Mar 15 17:32:28 2015 Valentin Cardon
-** Last update Sun Mar 15 17:36:59 2015 Valentin Cardon
+** Last update Sun Mar 15 17:57:06 2015 Valentin Cardon
 */
 
 #include	"elcrypt.h"
@@ -19,4 +19,19 @@ void		rotate_crypt(u_key *key)
   ttmp = (key->llong >> (56 - 4));
   key->llong = tmp | ttmp;
   key->cchar[7] = 0;
+}
+
+void		rotate_decrypt(u_key *key_prim, u_key *key_sec, int i)
+{
+  unsigned long	tmp;
+  unsigned long	ttmp;
+
+  key_sec->llong = key_prim->llong;
+  while (n > 1)
+    {
+      tmp = (key_sec->llong << 4);
+      ttmp = (key_sec->llon >>(56 - 4));
+      key_sec->llong = tmp | ttmp;
+      nn--;
+    }
 }
